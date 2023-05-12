a9300e606e422d2cb6799281683a99bad57f6cca
Андрей <ulsk0073@gmail.com>
2023-05-12 10:04:15 +0400

    80.fix add js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const header = document.querySelector(".header-container");
+const toggleClass = "is-sticky";
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll > 150) {
+    header.classList.add(toggleClass);
+  } else {
+    header.classList.remove(toggleClass);
+  }
+});
